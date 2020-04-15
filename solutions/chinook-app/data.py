5f99a051f0fbde9e462775cd2f56248fc09114a9
kunxin-chor <chorkunxin@yahoo.com>
2020-04-15 17:21:43 +0000

    Added some code

---
+++ solutions/chinook-app/data.py
@@ -0,0 +1,27 @@
+import pymysql
+import os
+
+# Optional - you can just set up environment variables in .bashrc
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE = "Chinook"
+
+def get_connection():
+    print("DB USER=",os.environ.get('DB_USER'))
+    connection = pymysql.connect(
+        host='localhost',
+        user=os.environ.get('C9_USER'),
+        password='',
+        database=DATABASE
+    )
+    return connection
+    
+def get_employees():
+    connection = get_connection()
+        
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    
+    sql = "SELECT * FROM Employee"
+    cursor.execute(sql)
+    return cursor
